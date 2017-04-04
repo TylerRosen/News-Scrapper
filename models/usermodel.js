eaa4040105c9b4d01113b737f85358f43e931307
TylerRosen <tylermrosen@gmail.com>
2017-04-04 16:31:35 -0400

    Server displays article title and links

--- models/usermodel.js
+++ models/usermodel.js
@@ -3,3 +3,11 @@ var mongoose = require("mongoose");
 
 // Create the Schema class
 var Schema = mongoose.Schema;
+
+// Requests HTML body of site
+request('https://theonion.com/', function(error, response, html) {
+
+    // Loads HTML and saves to variable
+    var url = cheerio.load(html);
+
+});
