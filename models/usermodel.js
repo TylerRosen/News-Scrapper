160cb1c8115118c443f1f567e451fae9ef0b374b
TylerRosen <tylermrosen@gmail.com>
2017-04-06 11:06:33 -0400

    Edited schema

--- models/usermodel.js
+++ models/usermodel.js
@@ -3,11 +3,3 @@ var mongoose = require("mongoose");
 
 // Create the Schema class
 var Schema = mongoose.Schema;
-
-// Requests HTML body of site
-request('https://theonion.com/', function(error, response, html) {
-
-    // Loads HTML and saves to variable
-    var url = cheerio.load(html);
-
-});
